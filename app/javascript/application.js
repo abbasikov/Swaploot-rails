f688275b24ac5080948adc7b03f7c693898054bc
Umair ABBASI <writeumair86@gmail.com>
2023-11-08 17:56:21 +0500

    nav bar and datatables

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+
+$( document ).ready(function() {
+    const userMenuButton = document.getElementById("user-menu-button");
+    const userDropdown = document.getElementById("user-dropdown");
+  
+    userMenuButton.addEventListener("click", () => {
+        userDropdown.classList.toggle("hidden");
+    });
+});
