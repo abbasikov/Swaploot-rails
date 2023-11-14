1147d6dfe700acc7d6eb40b1f7d84f218e7657b1
Umair ABBASI <writeumair86@gmail.com>
2023-11-14 19:08:46 +0500

    steam account edit update functionality

--- app/controllers/steam_accounts_controller.rb
+++ app/controllers/steam_accounts_controller.rb
@@ -1,4 +1,5 @@
 class SteamAccountsController < ApplicationController
+  before_action :set_steam_account, only: %i[edit update]
   def index
     @steam_accounts = SteamAccount.all
   end
@@ -17,7 +18,22 @@ class SteamAccountsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @steam_account.update(steam_account_params)
+      flash[:notice] = 'Steam account was successfully updated.'
+      redirect_to steam_accounts_path
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_steam_account
+    @steam_account = SteamAccount.find_by(id: params[:id])
+  end
 
   def steam_account_params
     params.require(:steam_account).permit(:steam_id, :unique_name,:steam_web_api_key, :waxpeer_api_key, :csgoempire_api_key, :market_csgo_api_key)
