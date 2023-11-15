768f675c74fe61dca392191aa88465749476049f
Umair ABBASI <writeumair86@gmail.com>
2023-11-15 12:34:58 -0800

    Fixed issue when changing accounts related to balance.

--- app/services/marketcsgo_service.rb
+++ app/services/marketcsgo_service.rb
@@ -5,28 +5,23 @@ class MarketcsgoService
 
   def initialize(current_user)
     @current_user = current_user
+    @active_steam_account = SteamAccount.find_by(active: true, user_id: @current_user.id)
+    @params = {
+      key: "#{@active_steam_account&.market_csgo_api_key}"
+    }
   end
 
   def fetch_my_inventory
-    @active_steam_account = SteamAccount.find_by(active: true, user_id: @current_user.id)
-    params = {
-      key: "#{@active_steam_account&.market_csgo_api_key}"
-    }
-    response = self.class.get(BASE_URL + '/my-inventory', query: params)
-
+    response = self.class.get(BASE_URL + '/my-inventory', query: @params)
     save_inventory(response)
   end
 
   def fetch_balance
-    params = {
-      key: "#{@active_steam_account&.market_csgo_api_key}"
-    }
-    res = self.class.get(BASE_URL + '/get-money', query: params)
+    res = self.class.get(BASE_URL + '/get-money', query: @params)
     res['money'] if res
   end
 
   def save_inventory(res)
-    @active_steam_account = SteamAccount.find_by(active: true, user_id: @current_user.id)
     if @active_steam_account
       res['items']&.each do |item|
         Inventory.find_or_create_by(item_id: item['id'], steam_id: @active_steam_account&.steam_id, market_name: item['market_hash_name'], market_price: item['market_price'], tradable: item['tradable'])
--- app/services/waxpeer_service.rb
+++ app/services/waxpeer_service.rb
@@ -5,26 +5,19 @@ class WaxpeerService
 
   def initialize(current_user)
     @current_user = current_user
+    @active_steam_account = SteamAccount.find_by(active: true, user_id: @current_user.id)
+    @params = {
+      api: @active_steam_account&.waxpeer_api_key
+    }
   end
 
   def fetch_active_trade
-    params = {
-      api: api_key
-    }
-    res = self.class.post(BASE_URL + '/my-history', query: params)
+    res = self.class.post(BASE_URL + '/my-history', query: @params)
     res['data'].present? ? res['data']['trades'] : []
   end
 
   def fetch_balance
-    params = {
-      api: api_key
-    }
-    res = self.class.get(BASE_URL + '/user', query: params)
+    res = self.class.get(BASE_URL + '/user', query: @params)
     res['user'].present? ? res['user']['wallet'].to_f / 1000 : 0
-  end
-
-  def api_key
-    @active_steam_account = SteamAccount.find_by(active: true, user_id: @current_user.id)
-    @active_steam_account&.waxpeer_api_key
   end
 end
