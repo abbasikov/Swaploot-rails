d910d741cf717d88aa6fedba796114554b330db5
Umair ABBASI <writeumair86@gmail.com>
2023-11-20 19:50:11 +0530

    fix formatting errors

--- app/services/marketcsgo_service.rb
+++ app/services/marketcsgo_service.rb
@@ -1,8 +1,7 @@
 class MarketcsgoService
-  include HTTParty
-  
-  BASE_URL = 'https://market.csgo.com/api/v2'
-  
+  include HTTParty  
+  BASE_URL = 'https://market.csgo.com/api/v2' 
+
   def initialize(current_user)
     @current_user = current_user
     @active_steam_account = SteamAccount.find_by(active: true, user_id: @current_user.id)
@@ -10,12 +9,12 @@ class MarketcsgoService
       key: "#{@active_steam_account&.market_csgo_api_key}"
     }
   end
-  
+
   def fetch_my_inventory
     response = self.class.get(BASE_URL + '/my-inventory', query: @params)
     save_inventory(response)
   end
-  
+
   def fetch_balance
     res = self.class.get(BASE_URL + '/get-money', query: @params)
     res['money'] if res
