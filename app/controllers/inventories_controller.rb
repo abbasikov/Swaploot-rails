b3f1501ee8aeb1a66e69817bfb6681fc1032471d
Umair ABBASI <writeumair86@gmail.com>
2023-11-28 20:14:50 +0530

    add tradable filters and total worth of inventory

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -3,7 +3,14 @@ class InventoriesController < ApplicationController
 
   def index
     @active_steam_account = current_user.active_steam_account
-    @inventories = Inventory.steam_inventories(@active_steam_account)
+    if params["tradable"] == "true"
+      @inventories = Inventory.tradable_steam_inventories(@active_steam_account)
+    elsif params["tradable"] == "false"
+      @inventories = Inventory.non_tradable_steam_inventories(@active_steam_account)
+    else
+      @inventories = Inventory.where(steam_id: @active_steam_account.steam_id)
+    end
+
     begin
       @missing_items = MissingItemsService.new(current_user).missing_items
     rescue StandardError => e
@@ -13,6 +20,7 @@ class InventoriesController < ApplicationController
       format.html
       format.js
     end
+    @total_market_price = Inventory.sum(:market_price)
   end
 
   private
--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -3,6 +3,12 @@ class Inventory < ApplicationRecord
   scope :steam_inventories, ->(active_steam_account) {
     where(steam_id: active_steam_account&.steam_id)
   }
+  scope :tradable_steam_inventories, ->(active_steam_account) {
+    where(steam_id: active_steam_account.steam_id, tradable: true)
+  }
+  scope :non_tradable_steam_inventories, ->(active_steam_account) {
+    where(steam_id: active_steam_account.steam_id, tradable: false)
+  }
 
   def soft_delete_and_set_sold_at
     update(sold_at: Time.current)
