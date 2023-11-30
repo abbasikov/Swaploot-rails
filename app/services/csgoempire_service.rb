e256dc6a8749056a9f7859049cb5cf2ec21e418b
Umair ABBASI <writeumair86@gmail.com>
2023-11-30 11:44:40 -0800

    soft deletion implementation

--- app/services/csgoempire_service.rb
+++ app/services/csgoempire_service.rb
@@ -21,8 +21,20 @@ class CsgoempireService
       # for now, pass dummy values i.e. max_percentage = 20, specific_price = 100
       CsgoEmpireBuyingInitiateJob.perform_later(@current_user, data['item_data'], 20, 100)
     end
+    if data['event'] == 'trade_status'
+      data['item_data'].each do |item|
+        if item['data']['status_message'] == 'Sent'
+          inventory = Inventory.find_by(item_id: item['data']['item_id'])
+          if inventory.present?
+            inventory.soft_delete_and_set_sold_at
+            # service_hash = set_remove_item_hash data
+            # RemoveItems.remove_item_from_all_services(@current_user, service_hash)
+          end
+        end
+      end
+    end
   end
-  
+
   def fetch_item_listed_for_sale
     return [] if csgoempire_key_not_found?
 
@@ -39,6 +51,24 @@ class CsgoempireService
     HTTParty.get(BASE_URL + '/metadata/socket', headers: headers)
   end
 
+  def fetch_my_inventory
+    return if csgoempire_key_not_found?
+
+    response = self.class.get(CSGO_EMPIRE_BASE_URL + '/trading/user/inventory', headers: @headers)
+    save_inventory(response)
+  end
+
+  def save_inventory(res)
+    if @active_steam_account
+      res['data']&.each do |item|
+        inventory = Inventory.find_by(item_id: item['id'])
+        unless inventory.present?
+          Inventory.create(item_id: item['id'], steam_id: @active_steam_account&.steam_id, market_name: item['market_name'], market_price: ((item['market_value'] / 100) * 0.164), tradable: item['tradable'])
+        end
+      end
+    end
+  end
+
   def fetch_active_trade
     return if csgoempire_key_not_found?
 
@@ -53,5 +83,25 @@ class CsgoempireService
 
   def csgoempire_key_not_found?
     @active_steam_account&.csgoempire_api_key.blank?
+  end
+
+  def set_remove_item_hash(data)
+    service_hash = { 'CsgoempireService': '', 'WaxpeerService': '' }
+    trade_service_info = data['item_data'].first
+    if trade_service_info['type'] == 'deposit' && trade_service_info.dig('data', 'status_message') == 'Sent'
+      service_hash['WaxpeerService'] = trade_service_info.dig('data', 'item', 'asset_id')
+    end
+
+    csgo_desposit_data = fetch_item_listed_for_sale
+    csgo_desposit_data.each do |record|
+      record['items'].each do |item_data|
+        if item_data['id'] == trade_service_info.dig('data', 'item_id')
+          service_hash['CsgoempireService'] = record['id']
+          break
+        end
+      end
+    end
+
+    service_hash
   end
 end
--- app/services/marketcsgo_service.rb
+++ app/services/marketcsgo_service.rb
@@ -8,29 +8,11 @@ class MarketcsgoService
     }
   end
 
-  def fetch_my_inventory
-    return if market_csgo_api_key_not_found?
-
-    response = self.class.get(MARKET_CSGO_BASE_URL + '/my-inventory', query: @params)
-    save_inventory(response)
-  end
-
   def fetch_balance
     return if market_csgo_api_key_not_found?
 
     res = self.class.get(MARKET_CSGO_BASE_URL + '/get-money', query: @params)
     res['money'] if res
-  end
-
-  def save_inventory(res)
-    if @active_steam_account
-      res['items']&.each do |item|
-        inventory = Inventory.find_by(item_id: item['id'])
-        unless inventory.present?
-          Inventory.create(item_id: item['id'], steam_id: @active_steam_account&.steam_id, market_name: item['market_hash_name'], market_price: item['market_price'], tradable: item['tradable'])
-        end
-      end
-    end
   end
 
   def market_csgo_api_key_not_found?
