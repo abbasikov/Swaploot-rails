3b920acb004936c533fcadc31648dd1c4d1e7457
Umair ABBASI <writeumair86@gmail.com>
2023-12-01 23:25:09 +0500

    Fixed Exception thrown due to dropdown in navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_steam_accounts
 
   rescue_from StandardError, with: :handle_error
 
   private
+
+  def set_steam_accounts
+    @steam_accounts = current_user.steam_accounts
+  end
 
   def handle_error(error)
     context = {
