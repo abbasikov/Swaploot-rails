42769e16ae8e666fd2742b6770bdca1261915fa1
Umair ABBASI <writeumair86@gmail.com>
2023-12-01 09:29:15 -0800

    final conflicts resolve

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -2,6 +2,7 @@ class InventoriesController < ApplicationController
   before_action :fetch_inventory, only: %i[index]
 
   def index
+    @steam_accounts = SteamAccount.where(user_id: current_user.id)
     @active_steam_account ||= current_user.active_steam_account.presence || current_user.steam_accounts
     if params["tradable"] == "true"
       @inventories = Inventory.tradable_steam_inventories(@active_steam_account)
