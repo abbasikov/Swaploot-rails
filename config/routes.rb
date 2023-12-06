57cf07e3ebbbfda17f9b8debda54b801d179b649
Umair ABBASI <writeumair86@gmail.com>
2023-12-06 17:49:43 +0500

    FIxed routes issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-   devise_for :users
   # Defines the root path route ("/")
   # root "articles#index"
   mount Sidekiq::Web => '/sidekiq'
