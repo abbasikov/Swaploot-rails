7f6e2b09eb227adbf44aef1c37d36238a3a0b3a3
Umair ABBASI <writeumair86@gmail.com>
2023-12-07 05:09:18 -0800

    rendering issue fixes

--- app/javascript/controllers/data_controller.js
+++ app/javascript/controllers/data_controller.js
@@ -6,6 +6,17 @@ import consumer from "../channels/consumer";
 export default class extends Controller {
   connect() {
     //this.startAllChannelsSubscriptions();
+    this.applyDarkModePreference();
+  }
+
+  applyDarkModePreference() {
+    const darkModePreference = localStorage.getItem('darkMode');
+    const body = document.body;
+    if (darkModePreference === 'true') {
+      body.classList.add('dark');
+    } else {
+      body.classList.remove('dark');
+    }
   }
 
   async startAllChannelsSubscriptions() {
