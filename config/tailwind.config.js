f3ad4dd688911498c1364e65d52a986ae16b03ab
Umair ABBASI <writeumair86@gmail.com>
2023-12-07 03:31:43 -0800

    Feature/UI enhancement (#107)
    * u fixes
    
    * u fixes
    
    * heading fixes
    
    * divider fixes

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,7 +12,7 @@ content: [
     extend: {
       colors: {
         blue: "rgb(37 99 235)",
-        blueLight:"#93c5fd",
+        blueLight:"#dbeafe",
         grayLight:"rgb(229 231 235)",
         gray:"rgb(156 163 175)",
         grayDark:"rgb(75 85 99)",
