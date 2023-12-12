fde64206f465a8af96e6d4a33744fcacea77a689
Umair ABBASI <writeumair86@gmail.com>
2023-12-12 09:05:09 -0800

    modal for edit api keys for steam accounts (#156) (#157) (#158)
    * modal for edit api keys for steam accounts
    
    * modal for edit api keys for steam accounts
    
    * discord message for successfull edit or try
    
    * discord message for successfull edit or try edited
    
    Co-authored-by: Umair ABBASI <writeumair86@gmail.com>

--- app/controllers/steam_accounts_controller.rb
+++ app/controllers/steam_accounts_controller.rb
@@ -1,5 +1,5 @@
 class SteamAccountsController < ApplicationController
-  before_action :set_steam_account, only: %i[edit update destroy show_api_keys]
+  before_action :set_steam_account, only: %i[edit update destroy show_api_keys edit_api_keys]
   after_action :set_steam_account_filters, only: %i[create]
   
   def index
@@ -39,6 +39,16 @@ class SteamAccountsController < ApplicationController
     @success = check_password?
     if current_user.discord_bot_token.present? && current_user.discord_channel_id.present?
       @success ? notify_discord("#{current_user.email} successfully accessed API keys") :  notify_discord("#{current_user.email} tried to access API keys")
+    end
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def edit_api_keys
+    @success = check_password?
+    if current_user.discord_bot_token.present? && current_user.discord_channel_id.present?
+      @success ? notify_discord("#{current_user.email} successfully accessed API keys") :  notify_discord("#{current_user.email} tried to edit API keys")
     end
     respond_to do |format|
       format.js
