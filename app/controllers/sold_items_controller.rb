a7cc1bb81179108f52d9c44e764b053ddee4d3ee
Umair ABBASI <writeumair86@gmail.com>
2023-12-19 23:28:12 +0500

    Pagination with remote request

--- app/controllers/sold_items_controller.rb
+++ app/controllers/sold_items_controller.rb
@@ -5,6 +5,10 @@ class SoldItemsController < ApplicationController
     steam_account = !@active_steam_account.respond_to?(:each) ? current_user.active_steam_account : current_user.steam_accounts
     @items_sold = SoldItem.where(steam_account: steam_account).paginate(page: params[:page], per_page: 15)
     @sold_items_history = SoldItemHistory.where(steam_account: steam_account).paginate(page: params[:sold_item_history_page], per_page: 15)
+    respond_to do |format|
+      format.js
+      format.html
+    end
   end
 
   def fetch_sold_items
