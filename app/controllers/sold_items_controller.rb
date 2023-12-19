8b24f2d8b091a3d6b93d060fa624647ea61acf88
app <app@Umar-Farooq.lan>
2023-12-19 18:00:55 +0500

    Ajax request pagination added

--- app/controllers/sold_items_controller.rb
+++ app/controllers/sold_items_controller.rb
@@ -4,7 +4,7 @@ class SoldItemsController < ApplicationController
   def index
     steam_account = !@active_steam_account.respond_to?(:each) ? current_user.active_steam_account : current_user.steam_accounts
     @items_sold = SoldItem.where(steam_account: steam_account).paginate(page: params[:page], per_page: 15)
-    @sold_items_history = steam_account.sold_item_histories.paginate(page: params[:sold_item_history_page], per_page: 15)
+    @sold_items_history = SoldItemHistory.where(steam_account: steam_account).paginate(page: params[:sold_item_history_page], per_page: 15)
   end
 
   def fetch_sold_items
@@ -14,7 +14,7 @@ class SoldItemsController < ApplicationController
 
       csgo_service = CsgoempireService.new(current_user)
       csgo_service.fetch_deposit_transactions
-      @sold_items_history = SoldItemHistory.all
+      @sold_items_history = SoldItemHistory.paginate(page: params[:sold_item_history_page], per_page: 15)
       respond_to do |format|
         format.js
       end
