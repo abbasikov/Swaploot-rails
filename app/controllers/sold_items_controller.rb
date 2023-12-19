c09595e370eea496144a9d3214055c961d1c60a0
Umair ABBASI <writeumair86@gmail.com>
2023-12-19 16:36:33 +0500

    Conficts resolved

--- app/controllers/sold_items_controller.rb
+++ app/controllers/sold_items_controller.rb
@@ -3,13 +3,9 @@ class SoldItemsController < ApplicationController
   # before_action :fetch_sold_items , only: [:index]
   def index
     @steam_accounts = SteamAccount.where(user_id: current_user.id)
-<<<<<<< HEAD
     steam_account = !@active_steam_account.respond_to?(:each) ? current_user.active_steam_account : current_user.steam_accounts
     @items_sold = SoldItem.where(steam_account: steam_account).paginate(page: params[:page], per_page: 15)
-=======
     @sold_items_history = SoldItemHistory.all
-    @items_sold = !@active_steam_account.respond_to?(:each) ? SoldItem.where(steam_account: current_user.active_steam_account) : SoldItem.where(steam_account: current_user.steam_accounts)
->>>>>>> f12aec7 (Sold Item History Done)
   end
 
   def fetch_sold_items
