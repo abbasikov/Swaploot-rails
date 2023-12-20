e922d0de3cb525e712d46426ef4341bfa56f691c
Umair ABBASI <writeumair86@gmail.com>
2023-12-20 22:39:14 +0500

    Feature/sellable inventory (#182)
    * Sellable Inventory, Handle 15 minutes Sellable Inventory upation, Update SellingCsgoService
    
    * cron job sellable inventory after 15 minutes
    
    * updated inventory controller for sellable inventory
    
    * Inventory Bulk Insertion
    
    * resolve conflicts
    
    * apply pagination that were left
    
    * pagination in steam accounts
    
    * update matching items for selling service csgo empire
    
    * update matching items for selling service csgo empire time zone issue
    
    ---------
    
    Co-authored-by: Umair ABBASI <writeumair86@gmail.com>

---
+++ app/services/sellable_inventory_updation_service.rb
@@ -0,0 +1,25 @@
+class SellableInventoryUpdationService < ApplicationService
+	include HTTParty
+
+	def initialize(steam_account)
+		@steam_account = steam_account
+	end
+
+	# function is to update sellable inventory from waxpeer API to update/BULK Insert database after 15 minutes
+	def update_sellable_inventory
+		headers = { 'Authorization' => "Bearer #{@steam_account.csgoempire_api_key}" }
+		begin
+			inventory_response = self.class.get(CSGO_EMPIRE_BASE_URL + '/trading/user/inventory', headers: headers)
+		rescue
+			puts "Something went wrong with Fetch inventory API.. retrying in 2 minutes"
+			SellableInventoryUpdationJob.perform_in(2.minutes, @steam_account.id)
+		end
+		if inventory_response['success'] == false
+			report_api_error(inventory_response, [self&.class&.name, __method__.to_s]) 
+		else
+			tradeable_inventory_to_save = inventory_response["data"].select { |item| item["market_value"] != -1 && item["tradable"] == true && item["tradelock"] == false }
+		end
+	end
+end
+  
+  
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,1 +1,8 @@
+every 5.minutes do
+  runner "SellableInventoryUpdationJob.perform_async", output: 'log/cron.log'
+end
 
+every 1.day, at: '12:00 am' do
+  runner "PermanentDeleteJob.perform_async"
+  runner "PriceEmpireSuggestedPriceJob.perform_async"
+end
