282eb91e0255278e5c220cf86ce0c9a7f99a9aed
Umair ABBASI <writeumair86@gmail.com>
2023-12-22 18:27:11 +0500

    Price empire API key moved to env file

--- app/jobs/price_empire_suggested_price_job.rb
+++ app/jobs/price_empire_suggested_price_job.rb
@@ -6,7 +6,7 @@ class PriceEmpireSuggestedPriceJob
         ActiveRecord::Base.transaction do
             PriceEmpire.destroy_all
             begin
-                response = HTTParty.get("https://api.pricempire.com/v3/items/prices?api_key=#{PRICEEMPIRE_API_KEY}&currency=USD&sources=buff&sources=waxpeer&sources=csgoempire")
+                response = HTTParty.get("https://api.pricempire.com/v3/items/prices?api_key=#{ENV['PRICEEMPIRE_API_KEY']}&currency=USD&sources=buff&sources=waxpeer&sources=csgoempire")
                 response.each do |item|
                     PriceEmpire.create!(item_name: item[0], liquidity: item[1]["liquidity"], buff: item[1]["buff"], waxpeer: item[1]["waxpeer"] )
                 end
