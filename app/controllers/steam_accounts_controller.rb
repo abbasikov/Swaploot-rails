304aec74d286cb16d63ba39e221ada76ce778158
Umair ABBASI <writeumair86@gmail.com>
2023-12-22 16:58:44 +0500

    Price Empire API column removed from steam account

--- app/controllers/steam_accounts_controller.rb
+++ app/controllers/steam_accounts_controller.rb
@@ -72,7 +72,7 @@ class SteamAccountsController < ApplicationController
 
 
   def steam_account_params
-    params.require(:steam_account).permit(:steam_id, :unique_name,:steam_web_api_key, :waxpeer_api_key, :csgoempire_api_key, :market_csgo_api_key, :price_empire_api_key)
+    params.require(:steam_account).permit(:steam_id, :unique_name,:steam_web_api_key, :waxpeer_api_key, :csgoempire_api_key, :market_csgo_api_key)
   end
 
   def set_steam_account_filters
