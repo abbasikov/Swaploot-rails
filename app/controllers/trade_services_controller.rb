d2aed42f62efbf320627ac0928afe4d07b9e1219
Umair ABBASI <writeumair86@gmail.com>
2023-12-27 19:27:16 +0500

    deletion jobs handled, price cutting

--- app/controllers/trade_services_controller.rb
+++ app/controllers/trade_services_controller.rb
@@ -42,7 +42,22 @@ class TradeServicesController < ApplicationController
   end
   
   def delete_enqueued_job(job_id)
-    job = Sidekiq::Queue.new("default").find_job(job_id)
+    queue = Sidekiq::Queue.new # For jobs in the queue
+    scheduled = Sidekiq::ScheduledSet.new # For scheduled jobs
+    retries = Sidekiq::RetrySet.new # For jobs in retry
+
+    # Check in the queue
+    job = queue.find_job(job_id)
+    job.delete if job
+
+    # Check in scheduled jobs
+    job = scheduled.find_job(job_id)
+    job.delete if job
+
+    # Check in retry set
+    job = retries.find_job(job_id)
     job.delete if job
   end
 end
+
+
