c50add593a66ff36bfeae9f96238d03725239735
Umair ABBASI <writeumair86@gmail.com>
2023-12-28 20:49:28 +0500

    price cutting logic resolve comments

--- app/jobs/price_empire_suggested_price_job.rb
+++ app/jobs/price_empire_suggested_price_job.rb
@@ -8,11 +8,9 @@ class PriceEmpireSuggestedPriceJob
             begin
                 # response = HTTParty.get("https://api.pricempire.com/v3/items/prices?api_key=012c1691-f511-4545-ab78-cf6b5b2b1b38&currency=USD&sources=buff&sources=waxpeer&sources=csgoempire")
                 response = HTTParty.get("https://api.pricempire.com/v3/items/prices?api_key=#{ENV['PRICEEMPIRE_API_KEY']}&currency=USD&sources=buff&sources=waxpeer&sources=csgoempire")
-                # if response["message"] == "success"
-                    response.each do |item|
-                        PriceEmpire.create!(item_name: item[0], liquidity: item[1]["liquidity"], buff: item[1]["buff"], waxpeer: item[1]["waxpeer"] )
-                    end
-                # end
+                response.each do |item|
+                    PriceEmpire.create!(item_name: item[0], liquidity: item[1]["liquidity"], buff: item[1]["buff"], waxpeer: item[1]["waxpeer"] )
+                end
             rescue
                 puts "Price Empire Job Failed..."
                 PriceEmpireSuggestedPriceJob.perform_in(2.minutes)
