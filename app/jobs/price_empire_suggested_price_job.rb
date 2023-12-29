e6a219ae85404cdb98b4aad7502c8120cf993809
Umair ABBASI <writeumair86@gmail.com>
2023-12-29 20:02:40 +0500

    Handled price empire failure case

--- app/jobs/price_empire_suggested_price_job.rb
+++ app/jobs/price_empire_suggested_price_job.rb
@@ -8,12 +8,14 @@ class PriceEmpireSuggestedPriceJob
             begin
                 # response = HTTParty.get("https://api.pricempire.com/v3/items/prices?api_key=012c1691-f511-4545-ab78-cf6b5b2b1b38&currency=USD&sources=buff&sources=waxpeer&sources=csgoempire")
                 response = HTTParty.get("https://api.pricempire.com/v3/items/prices?api_key=#{ENV['PRICEEMPIRE_API_KEY']}&currency=USD&sources=buff&sources=waxpeer&sources=csgoempire")
-                response.each do |item|
-                    PriceEmpire.create!(item_name: item[0], liquidity: item[1]["liquidity"], buff: item[1]["buff"], waxpeer: item[1]["waxpeer"] )
+                unless JSON.parse(response.body)["statusCode"] == 403
+                    response.each do |item|
+                        PriceEmpire.create!(item_name: item[0], liquidity: item[1]["liquidity"], buff: item[1]["buff"], waxpeer: item[1]["waxpeer"] )
+                    end
                 end
             rescue
                 puts "Price Empire Job Failed..."
-                PriceEmpireSuggestedPriceJob.perform_in(2.minutes)
+                PriceEmpireSuggestedPriceJob.perform_in(60.minutes)
             end
         end
     end
