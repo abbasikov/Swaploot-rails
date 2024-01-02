916c483ad2b601b5bc19dba1043911811a3756b0
Umair ABBASI <writeumair86@gmail.com>
2024-01-02 08:40:43 -0800

    added background notification using action cable

--- app/javascript/controllers/data_controller.js
+++ app/javascript/controllers/data_controller.js
@@ -4,8 +4,37 @@ import consumer from "../channels/consumer";
 
 // Connects to data-controller="csgo"
 export default class extends Controller {
+  static targets = ["user"]
+
   connect() {
     this.applyDarkModePreference();
+    const userId = this.userTarget.getAttribute("data-user-id");
+    this.subscribechannel(userId);
+  }
+
+  subscribechannel(userId){
+    if (userId){
+      consumer.subscriptions.create({
+        channel: "FlashMessagesChannel",
+        user_id: userId
+      }, {
+        connected() {
+          // Called when the subscription is ready for use on the server
+        },
+      
+        disconnected() {
+          // Called when the subscription has been terminated by the server
+        },
+      
+        received(data) {
+          // Called when there's incoming data on the websocket for this channel
+          $('#toast-success').removeClass('hidden').find('.success_message').text(data.message);
+          setTimeout(function() {
+            $('#toast-success').addClass('hidden');
+          }, 10000);
+        }
+      });
+    }
   }
 
   applyDarkModePreference() {
