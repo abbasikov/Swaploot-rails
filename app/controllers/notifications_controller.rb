3c9369caace2a45e30fbb01463b76f09c63ae1e8
Umair ABBASI <writeumair86@gmail.com>
2024-02-15 03:07:46 -0800

    file changed for deployment testing

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,13 +1,11 @@
 class NotificationsController < ApplicationController
+	def index
+		@notifications = current_user.notifications.order(updated_at: :desc).paginate(page: params[:page], per_page: 15)
+	end
 
-    def index
-        @notifications = current_user.notifications.order(updated_at: :desc).paginate(page: params[:page], per_page: 15)
-    end
-  
-    def mark_all_as_read
-        current_user.notifications.update_all(is_read: true)
-        flash[:notice] = "All notifications are marked as read."
-        redirect_to notifications_path
-    end
-  end
-  
+	def mark_all_as_read
+		current_user.notifications.update_all(is_read: true)
+		flash[:notice] = 'All notifications are marked as read.'
+		redirect_to notifications_path
+	end
+end
